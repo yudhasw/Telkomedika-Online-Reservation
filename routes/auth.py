20e09e1c2c6249f9f5f50850ff49b901b53ab761
Daisaq Hadya Albar <165605674+eXIA008@users.noreply.github.com>
2025-10-27 13:08:22 +0700

    rm routes directory

--- routes/auth.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from flask_login import login_user, logout_user, login_required
-
-bp = Blueprint("auth", __name__)
-
-@bp.route("/register", methods=["GET", "POST"])
-def register():
-  return
