c71d17b72ad8e12694cda5028e9d7839a4c0641b
yudhasw <yudha.sw2006@gmail.com>
2025-11-01 12:40:10 +0700

    ADD registration logic for auth

--- routes/auth.py
+++ routes/auth.py
@@ -8,28 +8,48 @@ auth_bp = Blueprint("auth", __name__)
 
 @auth_bp.route("/register", methods=["GET", "POST"])
 def register():
-  return
+    if request.method == 'POST':
+        nama = request.form.get('nama')
+        email = request.form.get('email')
+        nomor_hp = request.form.get('nomor_hp')
+        jenis_kelamin = request.form.get('jenis_kelamin')
+        tanggal_lahir = request.form.get('tanggal_lahir')
+        password = request.form.get('password')
+        confirmpassword = request.form.get('confirmpassword')
+
+        if Pasien.query.filter_by(email=email).first():
+          flash("Email sudah terdaftar!", "warning")
+          return render_template('register.html')
+
+        if password != confirmpassword:
+          flash("Password dan Confirm Passowrd tidak sama!", "warning")
+          return render_template('register.html')
+
+        # TODO: add user creation logic here (save to DB, hash password, etc.)
+        return render_template('index.html', success="Registration successful")
+
+    return render_template('signup.html')
 
 @auth_bp.route("/login", methods=["GET", "POST"])
 def login():
-  if request.method == 'POST':
-    email = request.form['email']
-    password = request.form['password']
+    if request.method == 'POST':
+        email = request.form.get('email', '')
+        password = request.form.get('password', '')
 
-    if "Admin@" in email:
-       user = Admin.query.filter_by(email=email).first()
-       role = 'admin'
-    else:
-       user = Pasien.query.filter_by(email=email).first()
-       role = 'user'
+        if "Admin@" in email:
+            user = Admin.query.filter_by(email=email).first()
+            role = 'admin'
+        else:
+            user = Pasien.query.filter_by(email=email).first()
+            role = 'user'
 
-    if user and password:
-        session['user_id'] = user.admin_id if role == 'admin' else user.pasien_id
-        flash('Login berhasil!')
-        return render_template('index.html')
-    else:
-        flash('Username atau password salah.')
+        if user and password:
+            session['user_id'] = user.admin_id if role == 'admin' else user.pasien_id
+            flash('Login berhasil!')
+            return render_template('index.html')
+        else:
+            flash('Username atau password salah.')
 
-  return render_template('login.html')
+    return render_template('login.html')
 
 # check_password_hash(user.password, password)
