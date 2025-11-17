a937697294a48ba40c079ff717d919137e1fdd62
yudhasw <yudha.sw2006@gmail.com>
2025-11-17 12:15:02 +0700

    Add: modoule testing

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from app import create_app, db
+
+@pytest.fixture
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
+    })
+
+    with app.app_context():
+        db.create_all()
+        yield app
+        db.drop_all()
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
