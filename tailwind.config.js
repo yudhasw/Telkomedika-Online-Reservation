859bbfacff7cb371107a4eeeec71e5a8aca4e7af
eXIA008 <165605674+eXIA008@users.noreply.github.com>
2025-11-19 06:04:35 +0700

    fix feat:login and register

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /**  {import('tailwindcss').Config} */
 export default {
-    content: ["./app/templates/**/*.html", "./app/static/src/**/*.css"],
+    content: [
+      "./app/templates/**/*.html", "./app/static/src/**/*.css",
+      "node_modules/preline/dist/*.js"
+    ],
     theme: {
       extend: {},
     },
-    plugins: [],
+    plugins: [
+      require('preline/plugin'),
+    ],
   };
